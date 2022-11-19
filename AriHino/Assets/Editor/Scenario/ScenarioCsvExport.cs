dbc5a379063d40a656ea6e42665947744e154881
windpow <windpow828@gmail.com>
2022-11-20 02:15:31 +0900

    ツールのフォルダ移動

---
+++ AriHino/Assets/Editor/Scenario/ScenarioCsvExport.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using CsvHelper;
+
+public class ScenarioCsvExport : EditorWindow
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
