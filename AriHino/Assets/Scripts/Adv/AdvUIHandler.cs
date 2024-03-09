6ba8b4993f856cfd3e3699b9add474e61a7ec76e
DESKTOP-A39OOA0\owner <kaza.ma33heart@gmail.com>
2024-03-09 16:04:06 +0900

    Booksの付箋挙動修正、ブラー処理追加

--- AriHino/Assets/Scripts/Adv/AdvUIHandler.cs
+++ AriHino/Assets/Scripts/Adv/AdvUIHandler.cs
@@ -8,6 +8,8 @@ public class AdvUIHandler : MonoBehaviour
 
     [SerializeField] private ObjectActivator booksActivator;
 
+    [SerializeField] private UIBlurHandler uiBlur;
+
     /// <summary>
     /// Booksの表示ボタン押下時の移動処理
     /// </summary>
@@ -17,10 +19,17 @@ public class AdvUIHandler : MonoBehaviour
             booksUiMover.MoveTransition();
             booksUiMover.gameObject.SetActive(true);
             booksActivator.ActiveChangeObject(true);
+            ActiveUiBlur(5f);
         }
         else {
             booksUiMover.ReturnPosition(() => booksUiMover.gameObject.SetActive(false));
             booksActivator.ActiveChangeObject(false);
+            ActiveUiBlur(0f);
         }
+    }
+
+    private void ActiveUiBlur(float value) {
+
+        uiBlur.ChangeMaterialValue(value);
     }
 }
---
+++ AriHino/Assets/Scripts/General/Util/UIBlurHandler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Microsoft.Unity.VisualStudio.Editor;
+
+public class UIBlurHandler : MonoBehaviour
+{
+    [SerializeField] private Material material;
+
+    [SerializeField] private float duration;
+
+    public void ChangeMaterialValue(float targetValue)
+    {
+        // DOTweenを使用してmaterialの値を変更するTweenを作成
+        material.DOFloat(targetValue, "_SamplingDistance", duration);
+    }
+
+}
